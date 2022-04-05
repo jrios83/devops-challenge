1d4b238f7e6764e4e36290d4a3dd99ab9aa2c0e6
jrios83 <jrios.orsis@gmail.com>
2022-04-05 18:54:30 -0500

    Adding ssh connection

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -43,9 +43,9 @@ resource "aws_security_group" "allow_lisp_ports" {
         cidr_blocks = ["0.0.0.0/0"]
     }
     egress{
-        from_port = 0
-        to_port = 0
-        protocol = "22"
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
         cidr_blocks = ["11.22.33.44/8"]
     }
 }
